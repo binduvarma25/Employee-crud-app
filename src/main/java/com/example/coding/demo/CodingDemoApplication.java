d094d975113a0b8917b71b52e09813d6e39c2ab8
Hima Bindu <hpenmetsa5@gmail.com>
2022-04-29 12:49:53 +0530

    Initial repo

---
+++ src/main/java/com/example/coding/demo/CodingDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.coding.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodingDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CodingDemoApplication.class, args);
+	}
+
+}
