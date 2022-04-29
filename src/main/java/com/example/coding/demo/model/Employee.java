cf97b39e7fc05575aae1b527fd8ace5a18bcc714
Hima Bindu <hpenmetsa5@gmail.com>
2022-04-29 13:09:01 +0530

    Added Bean Validation to the model/Employee.java

--- src/main/java/com/example/coding/demo/model/Employee.java
+++ src/main/java/com/example/coding/demo/model/Employee.java
@@ -5,6 +5,7 @@ import lombok.Data;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import javax.validation.constraints.*;
 import java.util.Date;
 
 @Entity
@@ -14,10 +15,16 @@ public class Employee {
     @Id
     @GeneratedValue
     private Integer id;
+    @NotBlank
     private String name;
+    @NotBlank
+    @Email
     private String email;
+    @NotBlank
     private double salary;
+    @PastOrPresent
     private Date joiningDate;
+    @NotBlank
     private String role;
 
 }
