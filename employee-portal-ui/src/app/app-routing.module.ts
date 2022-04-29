855a295ef2393916a202973e3f388194bb9f83b2
Hima Bindu <hpenmetsa5@gmail.com>
2022-04-29 20:16:05 +0530

    Routing enabled

--- employee-portal-ui/src/app/app-routing.module.ts
+++ employee-portal-ui/src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AppComponent } from './app.component';
+import { AddEmployeeComponent } from './employee/add-employee/add-employee.component';
+import { ListEmmployeeComponent } from './employee/list-emmployee/list-emmployee.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: AppComponent },
+  { path: 'add', component: AddEmployeeComponent },
+  { path: 'list', component: ListEmmployeeComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- employee-portal-ui/src/app/app.component.ts
+++ employee-portal-ui/src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.sass']
 })
 export class AppComponent {
-  title = 'employee-portal-ui';
+  title = 'Employee Portal';
 }
